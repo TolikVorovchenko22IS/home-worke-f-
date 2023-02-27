127b461ae2e90ff9876f07803fa29acce08e5492
TolikVorovchenko22IS <124189191+TolikVorovchenko22IS@users.noreply.github.com>
2023-02-27 08:41:13 +0300

    Add files via upload

---
+++ Program.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+
+
+Imports System.Linq.Expressions
+Imports System.Runtime.Intrinsics.Arm
+Public Sub Main(args As String(), console As Console)
+    If console Is Nothing Then
+        Throw New ArgumentNullException(NameOf(console))
+    End If
+
+    Dim p, i As String
+    Console.WriteLine(" Óìåþ ñ÷èòàòü ïðîèçâåäåíèå")
+    Console.WriteLine("Íàïèøè 1 åñëè òû çàêîí÷èë ")
+    i = 0
+    p = 1
+    While (i <> 1)
+    End While
+
+    i = CInt(Console.ReadLine())
+    p = p * i
+    If (i = 1) Then
+
+
+    End If
+    If p > (Int64.MaxValue / p) Then
+        Console.Write(" Ïàìÿòü ïåðåãðóæèíà !!! = ")
+        Console.Write(p)
+
+    End If
+
+End Sub
